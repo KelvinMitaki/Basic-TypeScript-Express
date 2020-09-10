421a320db963cddd847b963580107c8cebc6300e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-11 00:29:09 +0300

    added router to controller

--- src/controllers/decorators/controllers.ts
+++ src/controllers/decorators/controllers.ts
@@ -1,6 +1,11 @@
+import { Router } from "express";
+
+export const controllerRouter = Router();
+
 export const controller = (routePrefix: string) => (target: Function) => {
   for (let key in target.prototype) {
     const routeHandler = target.prototype[key];
     const path = Reflect.getMetadata("path", target.prototype, key);
+    controllerRouter.get(`${routePrefix}${path}`, routeHandler);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from "express";
 import route from "./routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
+import "./controllers/LoginController";
+import { controllerRouter } from "./controllers/decorators/controllers";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["lschkjghskc"] }));
 app.use(route);
+app.use(controllerRouter);
 app.listen(3000, (): void => console.log("server started on port 3000"));
