95b5f23c915e619aef35bc131843f0c8a384266a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-11 00:56:21 +0300

    added a route binder in routes

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,9 +1,15 @@
 import "reflect-metadata";
 
-export const get = (path: string) => (
+const routeBinder = (method: string) => (path: string) => (
   target: any,
   key: string,
   desc: PropertyDescriptor
 ) => {
   Reflect.defineMetadata("path", path, target, key);
+  Reflect.defineMetadata("method", method, target, key);
 };
+
+export const get = routeBinder("get");
+export const put = routeBinder("put");
+export const patch = routeBinder("patch");
+export const del = routeBinder("delete");
